fc60e622d455a94e1a856ef3b730b55516df2287
Jay Péclard <jay.peclard@gmail.com>
2019-10-26 08:27:17 +0200

    added news, fix issues

--- components/Question.js
+++ components/Question.js
@@ -4,26 +4,30 @@ import Card from './Card';
 
 export default function Question(props) {
   return (
-    <Card>
-      <View style={{
-        overflow: 'hidden',
-        borderTopLeftRadius: 8,
-        borderTopRightRadius: 8,
-      }}>
-        <Image
-          style={{
-            flex: 1,
-            height: 150,
-            width: '100%',
-            resizeMode: 'cover'
-          }}
-          source={props.image}/>
-      </View>
-      <View style={{
-        padding: 10
-      }}>
-        {props.children}
-      </View>
-    </Card>
+    <View style={{
+      paddingBottom:10
+    }}>
+      <Card>
+        <View style={{
+          overflow: 'hidden',
+          borderTopLeftRadius: 8,
+          borderTopRightRadius: 8,
+        }}>
+          <Image
+            style={{
+              flex: 1,
+              height: 150,
+              width: '100%',
+              resizeMode: 'cover'
+            }}
+            source={props.image}/>
+        </View>
+        <View style={{
+          padding: 10
+        }}>
+          {props.children}
+        </View>
+      </Card>
+    </View>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,8 +3,13 @@ import {
   SafeAreaView,
   ScrollView,
   View,
+  Button,
 } from 'react-native';
 import Questions from '../components/Questions';
+import { AppTitle } from '../components/StyledText'
+import News from '../components/News';
+import Question from '../components/Question';
+import { AppText } from './EnvironmentScreen';
 
 export default function HomeScreen() {
   return (
@@ -17,7 +22,21 @@ export default function HomeScreen() {
           padding: 10,
         }}>
         <SafeAreaView>
+          <View style={{ paddingBottom: 10 }}>
+            <AppTitle>Daily Tips</AppTitle>
+          </View>
           <Questions/>
+          <View style={{ paddingBottom: 10 }}>
+            <AppTitle>News</AppTitle>
+          </View>
+          <Question
+            image={require('../assets/images/gewobag.jpg')}>
+            <AppTitle>Reuse Serverheat to heat apartments</AppTitle>
+            <AppText>EWZ distributes waste heat to 611 apartments in Zürich Altstetten from its server farms. Waste heat is 80% CO2 free! These 611 apartments save 1120 Tons of CO₂ per year compared to using fossil fuels.</AppText>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              <Button title='read more'/>
+            </View>
+          </Question>
         </SafeAreaView>
       </ScrollView>
     </View>
